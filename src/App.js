baf9798e0fcc954a222ec91bf08d71363bb0bf79
Daniel <danielvillarreal862@gmail.com>
2020-09-12 16:42:05 -0300

    carrito de compra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Footer from './component/footer';
+import Header from './component/header';
+import Producto from './component/producto';
+import Carrito from './component/carrito';
 
 function App() {
+
+  //crear listado de producto
+  const [productos, guardarProductos] = useState([
+    {id: 1, nombre: 'Camisa ReactJS', precio: 50},
+    {id: 2, nombre: 'Camisa VueJS', precio: 40},
+    {id: 3, nombre: 'Camisa Node.Js', precio: 30},
+    {id: 4, nombre: 'Camisa Angular', precio: 20},
+  ]);
+
+  //State para un carrito de compra
+const [carrito, agregarProducto] = useState([]);
+
+   //obtener la fecha
+   const fecha = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header
+     titulo='Tienda Virtual'
+     />
+      
+          <h1>Lista de Productos</h1>
+          {productos.map(producto => (
+            <Producto 
+              key={producto.id}
+              producto={producto}
+              productos={productos}
+              carrito={carrito}
+              agregarProducto={agregarProducto}
+            />
+          ))}
+   
+      <Carrito 
+      carrito={carrito}
+      agregarProducto={agregarProducto}
+      />
+ 
+     <Footer 
+     fecha={fecha}
+     />
+    </>
   );
 }
 
---
+++ src/component/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = ({fecha}) => {
+  
+    return (
+        <>
+        <footer>
+            <p>Todos los derechos reservados &copy {fecha};</p>
+   
+        </footer>
+        </>
+    );
+}
+ 
+export default Footer;
